b7b975835800474b595aa7936e44cd08390a2856
Predrag Njegovanovic <djaps94@gmail.com>
2021-12-14 14:56:22 +0100

    Implement getMetadata lambda (#1)
    * Include tiny-http-json as dependency
    
    * Implement get metadata lambda
    
    * Add dynamo table and env variables for metadata lambda
    
    * Include dynamodb unmarshaller and finalize getMetadata lambda function

--- functions/getMetadata/index.js
+++ functions/getMetadata/index.js
@@ -1,26 +1,59 @@
-const {
-  DynamoDBClient,
-  ListTablesCommand,
-} = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+const tiny = require('tiny-json-http');
 
-exports.handler = async (event, context) => {
-  const client = new DynamoDBClient({ region: "eu-central-1" });
-  const command = new ListTablesCommand({});
+const baseMetadataURI = 'https://ipfs.io/ipfs';
 
-  var results = {};
-  try {
-    results = await client.send(command);
-    console.log(results.TableNames.join("\n"));
-  } catch (err) {
-    console.error(err);
-  }
-
+const response = (statusCode, jsonData) => {
   return {
-    statusCode: 200,
+    statusCode: statusCode,
     headers: {
       "Content-Type": "application/json",
     },
     isBase64Encoded: false,
-    body: results,
+    body: jsonData,
   };
+};
+
+exports.handler = async (event, context) => {
+  const dynamodb = new DynamoDBClient({ region: process.env.REGION });
+  const tableName = process.env.STORAGE_NAME;
+  const partitionKeyName = "TokenId";
+
+  var params = {};
+    try {
+      params[partitionKeyName] = {"N": event.pathParameters.id};
+    } catch(err) {
+      return response(500, JSON.stringify({error: 'Id value has wrong type. ' + err}));
+    }
+
+    let getItemParams = {
+      TableName: tableName,
+      Key: params,
+      ProjectionExpression: "metadata"
+    }
+    const getCommand = new GetItemCommand(getItemParams);
+    try {
+      const data = await dynamodb.send(getCommand);
+      if (data.Item == undefined || data.Item == null) {
+        return response(404, JSON.stringify({error: `Could not find item with id: ${event.pathParameters.id}`}));
+      } else {
+        const metadataItem = unmarshall(data.Item);
+
+        const metadataHash = metadataItem.metadata;
+        const metadataURI = `${process.env.METADATA_URI}/${metadataHash}`;
+
+        console.info('Sending request to: ' + metadataURI);
+
+        try {
+            const res = await tiny.get({url: metadataURI});
+            return response(200, JSON.stringify(res.body));
+        } catch (error) {
+            console.log(error);
+            return response(500, JSON.stringify({error: 'Unable to fetch metadata for tokenId: ' + event.pathParameters.id}));
+        }
+      }
+    } catch (err) {
+      return response(500, JSON.stringify({ error: err.message }));
+    };
 };
